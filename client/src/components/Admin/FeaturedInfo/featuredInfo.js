4cf945613e822793811c7d75f4f5739e6fc48427
thucmanh <ttthuc28@gmail.com>
2021-06-14 03:36:13 +0700

    fix dât

--- client/src/components/Admin/FeaturedInfo/featuredInfo.js
+++ client/src/components/Admin/FeaturedInfo/featuredInfo.js
@@ -6,31 +6,31 @@ function featuredInfo() {
     return (
         <div className="featured">
             <div className="featuredItem">
-                <span className="featuredTitle">Revanue</span>
+                <span className="featuredTitle">Total Users</span>
                 <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">$2,415</span>
+                    <span className="featuredMoney">20</span>
                     <span className="featuredMoneyRate">
-                        -11.4 <ArrowDownward className="featuredIcon negative" />
+                        +6 <ArrowUpward className="featuredIcon" />
                     </span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
             </div>
             <div className="featuredItem">
-                <span className="featuredTitle">Sales</span>
+                <span className="featuredTitle">Total Posts</span>
                 <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">$4,415</span>
+                    <span className="featuredMoney">113</span>
                     <span className="featuredMoneyRate">
-                        -1.4 <ArrowDownward className="featuredIcon negative" />
+                        +60 <ArrowUpward className="featuredIcon" />
                     </span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
             </div>
             <div className="featuredItem">
-                <span className="featuredTitle">Cost</span>
+                <span className="featuredTitle">Total Reacts</span>
                 <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">$2,225</span>
+                    <span className="featuredMoney">1261</span>
                     <span className="featuredMoneyRate">
-                        +2.4 <ArrowUpward className="featuredIcon" />
+                        +315 <ArrowUpward className="featuredIcon" />
                     </span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
